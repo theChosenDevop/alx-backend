3ade2313a7e2999972b128ea78a31cf90660a1a2
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-21 21:41:21 +0000

    requirement checks

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -22,4 +22,6 @@ class BasicCache(BasicCaching):
     def get(self, key):
         """ Get an item by key
         """
+        if key is None or key not in self.cache_data:
+            return None
         return self.cache_data.get(key)
