4b596470cdf52a899d1a0c3d6e3541f7878aefd0
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-28 21:22:55 -0700

    corrected 2-app module

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,7 +18,7 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Determines the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
