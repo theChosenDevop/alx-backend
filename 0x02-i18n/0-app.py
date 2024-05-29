91e515d602b6192c05a2ddb58be63c7f9b0fd3b9
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-28 18:45:20 -0700

    update app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/'):
     """routes to the index page"""
     def index():
-        title =  “Welcome to Holberton”
+        title =“Welcome to Holberton”
         return render_template('template/index.html', title=title)
 
 if __name__ == "__main__":
