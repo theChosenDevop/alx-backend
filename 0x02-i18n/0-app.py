e6846621be406dfc0f63a2fccfd6832f5311df6e
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-28 18:54:37 -0700

    update 0-app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """0-app module"""
-from flask import Flask, rebder_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/'):
+@app.route('/')
+def index():
     """routes to the index page"""
-    def index():
-        title =“Welcome to Holberton”
-        return render_template('template/index.html', title=title)
+    title = "Welcome to Holberton"
+    return render_template('template/index.html', title=title)
 
 if __name__ == "__main__":
     app.run(debug=True)
